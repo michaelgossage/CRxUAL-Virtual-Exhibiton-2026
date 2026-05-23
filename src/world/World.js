6ebb59b7005d0ef31c02ff05d0f1ddedb5391b5a
michaelgossage <michaelgossage@hotmail.com>
2026-05-23 07:25:28 +0100

    test change

--- src/world/World.js
+++ src/world/World.js
@@ -1130,7 +1130,7 @@ this.setLocationRevealZone("EagleBar", { center: [1,23,12.8],     radius: 18});
       rotation: [0, -135, 0],
       normalizeTo: 0.8,
       clickable: true,
-      text: "",
+      //text: "",
       textOffset: [0, -0.1, 0.9],
       hitboxSize: [1.4, 1.4, 1.4],
       offsetClick: -0.3,
